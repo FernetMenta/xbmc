769089076e2d335ec601956728d28e19887871cb
FernetMenta <fernetmenta@online.de>
2011-12-19 09:07:53 +0100

    xvba: output, fix compile error if xvba is disabled

--- xbmc/cores/VideoRenderers/LinuxRendererGL.cpp
+++ xbmc/cores/VideoRenderers/LinuxRendererGL.cpp
@@ -2569,6 +2569,7 @@ void CLinuxRendererGL::UploadXVBATexture(int index)
 //********************************************************************************************************
 void CLinuxRendererGL::DeleteXVBAyv12Texture(int index)
 {
+#ifdef HAVE_LIBXVBA
   YV12Image &im     = m_buffers[index].image;
   YUVFIELDS &fields = m_buffers[index].fields;
   GLuint    *pbo    = m_buffers[index].pbo;
@@ -2616,10 +2617,12 @@ void CLinuxRendererGL::DeleteXVBAyv12Texture(int index)
       im.plane[0] = NULL;
     }
   }
+#endif
 }
 
 bool CLinuxRendererGL::CreateXVBAyv12Texture(int index)
 {
+#ifdef HAVE_LIBXVBA
   /* since we also want the field textures, pitch must be texture aligned */
   unsigned p;
 
@@ -2765,11 +2768,13 @@ bool CLinuxRendererGL::CreateXVBAyv12Texture(int index)
   }
   glDisable(m_textureTarget);
   m_eventTexturesDone[index]->Set();
+#endif
   return true;
 }
 
 void CLinuxRendererGL::UploadXVBAyv12Texture(int source)
 {
+#ifdef HAVE_LIBXVBA
   YUVBUFFER& buf       =  m_buffers[source];
   YV12Image* im        = &buf.image;
   YUVFIELDS& fields    =  buf.fields;
@@ -2853,6 +2858,7 @@ void CLinuxRendererGL::UploadXVBAyv12Texture(int source)
   CalculateTextureSourceRects(source, 3);
 
   glDisable(m_textureTarget);
+#endif
 }
 
 void CLinuxRendererGL::UploadYUV422PackedTexture(int source)
