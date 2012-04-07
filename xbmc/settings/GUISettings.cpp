e04457fc3617abce42e26dad52b3490d7462d2a2
xbmc <fernetmenta@online.de>
2012-04-07 09:19:00 +0200

    vdpau: redesign

--- xbmc/settings/GUISettings.cpp
+++ xbmc/settings/GUISettings.cpp
@@ -721,6 +721,8 @@ void CGUISettings::Initialize()
 
 #ifdef HAVE_LIBVDPAU
   AddBool(vp, "videoplayer.usevdpau", 13425, true);
+  AddBool(vp, "videoplayer.usevdpauinterop", 13436, true);
+  AddBool(vp, "videoplayer.usevdpauinteropyuv", 13437, false);
 #endif
 #ifdef HAVE_LIBVA
   AddBool(vp, "videoplayer.usevaapi", 13426, true);
