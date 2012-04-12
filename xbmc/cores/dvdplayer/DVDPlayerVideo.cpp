88741e19094cf4096f29a36a5540efc13372c65f
xbmc <fernetmenta@online.de>
2012-04-12 12:09:31 +0200

    xvba: add decoder

--- xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
+++ xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
@@ -1158,6 +1158,9 @@ int CDVDPlayerVideo::OutputPicture(const DVDVideoPicture* src, double pts)
       case RENDER_FMT_NONE:
         formatstr = "NONE";
         break;
+      case RENDER_FMT_XVBA:
+        formatstr = "XVBA";
+        break;
     }
 
     if(m_bAllowFullscreen)
