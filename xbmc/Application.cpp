e2ebac438e7a01e79d4c046c38f00e72b05e737b
xbmc <fernetmenta@online.de>
2012-07-26 15:43:24 +0200

    move deleting gl textures to TextureManager, fixes asic hang on AMD

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -2340,8 +2340,6 @@ void CApplication::Render()
 
   g_Windowing.EndRender();
 
-  g_TextureManager.FreeUnusedTextures();
-
   // reset our info cache - we do this at the end of Render so that it is
   // fresh for the next process(), or after a windowclose animation (where process()
   // isn't called)
@@ -2378,6 +2376,8 @@ void CApplication::Render()
     g_renderManager.NotifyDisplayFlip();
   }
   CTimeUtils::UpdateFrameTime(flip);
+
+  g_TextureManager.FreeUnusedTextures();
 
   g_renderManager.UpdateResolution();
   g_renderManager.ManageCaptures();
