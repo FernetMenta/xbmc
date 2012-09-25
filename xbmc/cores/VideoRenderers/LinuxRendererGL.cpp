874a8081b05c1c484a6d3fa61283a30dfc1fa0d5
xbmc <fernetmenta@online.de>
2012-09-25 12:14:15 +0200

    linuxrenderer: drop method RenderMultiPass

--- xbmc/cores/VideoRenderers/LinuxRendererGL.cpp
+++ xbmc/cores/VideoRenderers/LinuxRendererGL.cpp
@@ -1222,7 +1222,8 @@ void CLinuxRendererGL::Render(DWORD flags, int renderBuffer)
       break;
 
     case RQ_MULTIPASS:
-      RenderMultiPass(renderBuffer, m_currentField);
+      RenderToFBO(renderBuffer, m_currentField);
+      RenderFromFBO();
       VerifyGLState();
       break;
     }
@@ -1343,12 +1344,6 @@ void CLinuxRendererGL::RenderSinglePass(int index, int field)
   glMatrixMode(GL_MODELVIEW);
 
   VerifyGLState();
-}
-
-void CLinuxRendererGL::RenderMultiPass(int index, int field)
-{
-  RenderToFBO(index, field);
-  RenderFromFBO();
 }
 
 void CLinuxRendererGL::RenderToFBO(int index, int field)
