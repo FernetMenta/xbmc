b7bcf54bda69faefd6b178a44668b9b3f149020f
xbmc <fernetmenta@online.de>
2013-04-27 10:47:01 +0200

    ffmpeg: add methods to the interface needed for resampling

--- lib/DllSwResample.h
+++ lib/DllSwResample.h
@@ -60,6 +60,7 @@ public:
   virtual int swr_init(struct SwrContext *s)=0;
   virtual void swr_free(struct SwrContext **s)=0;
   virtual int swr_convert(struct SwrContext *s, uint8_t **out, int out_count, const uint8_t **in , int in_count)=0;
+  virtual int64_t swr_get_delay(struct SwrContext *s, int64_t base) = 0;
 };
 
 #if (defined USE_EXTERNAL_FFMPEG) || (defined TARGET_DARWIN) 
@@ -84,6 +85,7 @@ public:
   virtual int swr_init(struct SwrContext *s) { return ::swr_init(s); }
   virtual void swr_free(struct SwrContext **s){ return ::swr_free(s); }
   virtual int swr_convert(struct SwrContext *s, uint8_t **out, int out_count, const uint8_t **in , int in_count){ return ::swr_convert(s, out, out_count, in, in_count); }
+  virtual int64_t swr_get_delay(struct SwrContext *s, int64_t base) { return ::swr_get_delay(s, base); }
 };
 #else
 // Wrap the same API through libavresample.
@@ -114,6 +116,7 @@ public:
   virtual int swr_init(struct SwrContext *s) { return ::avresample_open(s); }
   virtual void swr_free(struct SwrContext **s){ ::avresample_close(*s); *s = NULL; }
   virtual int swr_convert(struct SwrContext *s, uint8_t **out, int out_count, const uint8_t **in , int in_count){ return ::avresample_convert(s, out, 0, out_count, (uint8_t**)in, 0,in_count); }
+  virtual int64_t swr_get_delay(struct SwrContext *s, int64_t base){::swr_get_delay(s, base);}
 };
 #endif
 
@@ -129,12 +132,14 @@ class DllSwResample : public DllDynamic, DllSwResampleInterface
   DEFINE_METHOD1(int, swr_init, (struct SwrContext *p1))
   DEFINE_METHOD1(void, swr_free, (struct SwrContext **p1))
   DEFINE_METHOD5(int, swr_convert, (struct SwrContext *p1, uint8_t **p2, int p3, const uint8_t **p4, int p5))
+  DEFINE_METHOD2(int64_t, swr_get_delay, (struct SwrContext *p1, int64_t p2))
 
   BEGIN_METHOD_RESOLVE()
     RESOLVE_METHOD(swr_alloc_set_opts)
     RESOLVE_METHOD(swr_init)
     RESOLVE_METHOD(swr_free)
     RESOLVE_METHOD(swr_convert)
+    RESOLVE_METHOD(swr_get_delay)
   END_METHOD_RESOLVE()
 
   /* dependencies of libavformat */
