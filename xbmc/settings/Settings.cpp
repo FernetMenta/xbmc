7b96d898f51577b0bd632d859e5ba258b2a212d0
unknown <fernetmenta@online.de>
2013-07-16 17:06:03 +0200

    AE: add streamsilence to gui settings for engines which suppport drain

--- xbmc/settings/Settings.cpp
+++ xbmc/settings/Settings.cpp
@@ -760,6 +760,9 @@ void CSettings::InitializeConditions()
   if (g_application.IsStandAlone())
     m_settingsManager->AddCondition("isstandalone");
 
+  if (CAEFactory::SupportsDrain())
+    m_settingsManager->AddCondition("audiosupportsdrain");
+
   // add more complex conditions
   m_settingsManager->AddCondition("addonhassettings", AddonHasSettings);
   m_settingsManager->AddCondition("checkmasterlock", CheckMasterLock);
@@ -852,6 +855,7 @@ void CSettings::InitializeISettingCallbacks()
   settingSet.insert("audiooutput.multichannellpcm");
   settingSet.insert("audiooutput.audiodevice");
   settingSet.insert("audiooutput.passthroughdevice");
+  settingSet.insert("audiooutput.streamsilence");
   settingSet.insert("lookandfeel.skin");
   settingSet.insert("lookandfeel.skinsettings");
   settingSet.insert("lookandfeel.font");
