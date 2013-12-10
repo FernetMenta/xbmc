a12b841386365d42cf4c403c033e651fd04f9c31
Rainer Hochecker <fernetmenta@online.de>
2013-12-10 13:48:55 +0100

    ActiveAE: suspend hdmi audio during change of refresh rate

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
@@ -234,6 +234,8 @@ void CActiveAE::StateMachine(int signal, Protocol *port, Message *msg)
         case CActiveAEControlProtocol::KEEPCONFIG:
           m_extKeepConfig = *(unsigned int*)msg->data;
           return;
+        case CActiveAEControlProtocol::DISPLAYRESET:
+          return;
         default:
           break;
         }
@@ -630,6 +632,17 @@ void CActiveAE::StateMachine(int signal, Protocol *port, Message *msg)
           {
             (*buffer)->Return();
           }
+          return;
+        default:
+          break;
+        }
+      }
+      else if (port == NULL) // timeout
+      {
+        switch (signal)
+        {
+        case CActiveAEControlProtocol::TIMEOUT:
+          m_extTimeout = 1000;
           return;
         default:
           break;
@@ -2257,12 +2270,12 @@ void CActiveAE::KeepConfiguration(unsigned int millis)
 
 void CActiveAE::OnLostDevice()
 {
-//  m_controlPort.SendOutMessage(CActiveAEControlProtocol::DISPLAYLOST);
+  m_controlPort.SendOutMessage(CActiveAEControlProtocol::DISPLAYLOST);
 }
 
 void CActiveAE::OnResetDevice()
 {
-//  m_controlPort.SendOutMessage(CActiveAEControlProtocol::DISPLAYRESET);
+  m_controlPort.SendOutMessage(CActiveAEControlProtocol::DISPLAYRESET);
 }
 
 //-----------------------------------------------------------------------------
