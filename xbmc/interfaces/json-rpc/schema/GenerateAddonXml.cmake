notspiff/kodi-cmake

cmake buildsystem

xbmc/interfaces/json-rpc/schema/GenerateAddonXml.cmake
@@ -1,0 +1,5 @@
+file(STRINGS ${XBMC_SOURCE_DIR}/xbmc/interfaces/json-rpc/schema/version.txt jsonrpc_version)
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${XBMC_BINARY_DIR}/addons/xbmc.json/addon.xml)
+configure_file(${XBMC_SOURCE_DIR}/addons/xbmc.json/addon.xml.in
+               ${XBMC_BINARY_DIR}/addons/xbmc.json/addon.xml @ONLY)

FernetMenta/xbmc

xbmc/interfaces/json-rpc/schema/GenerateAddonXml.cmake
@@ -1,0 +1,5 @@
+file(STRINGS ${CORE_SOURCE_DIR}/xbmc/interfaces/json-rpc/schema/version.txt jsonrpc_version)
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${CORE_BINARY_DIR}/addons/xbmc.json/addon.xml)
+configure_file(${CORE_SOURCE_DIR}/addons/xbmc.json/addon.xml.in
+               ${CORE_BINARY_DIR}/addons/xbmc.json/addon.xml @ONLY)