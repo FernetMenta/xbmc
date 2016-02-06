d04278934910d8762397fe878dd79e6c96abb25f
xbmc <fernetmenta@online.de>
2016-02-06 13:38:35 +0100

    add missing files

---
+++ xbmc/addons/InputStream.h
@@ -0,0 +1,46 @@
+/*
+ *      Copyright (C) 2016 Team Kodi
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+#include "AddonDll.h"
+#include "include/kodi_inputstream_types.h"
+
+typedef DllAddon<InputStream, INPUTSTREAM_PROPS> DllInputStream;
+namespace ADDON
+{
+  typedef CAddonDll<DllInputStream, InputStream, INPUTSTREAM_PROPS> InputStreamDll;
+
+  class CInputStream : public InputStreamDll
+  {
+  public:
+    CInputStream(const AddonProps &props)
+      : InputStreamDll(props)
+      , m_context{nullptr}
+    {};
+    CInputStream(const cp_extension_t *ext);
+    virtual ~CInputStream() {}
+    virtual AddonPtr Clone() const;
+
+  protected:
+    std::string m_extension;
+    std::string m_mimetype;
+    void* m_context;
+  };
+
+} /*namespace ADDON*/
