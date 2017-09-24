2903e23d879438e6f505e5f8e595303f9162fabc
Rainer Hochecker <fernetmenta@online.de>
2017-09-24 10:02:45 +0200

    VideoPlayer: expose stereo mdoe to dataCache

--- xbmc/cores/VideoPlayer/VideoPlayer.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -3678,7 +3678,6 @@ bool CVideoPlayer::OpenVideoStream(CDVDStreamInfo& hint, bool reset)
     if (!player->OpenStream(hint))
       return false;
 
-    s.stereo_mode = static_cast<IDVDStreamPlayerVideo*>(player)->GetStereoMode();
     if (s.stereo_mode == "mono")
       s.stereo_mode = "";
 
