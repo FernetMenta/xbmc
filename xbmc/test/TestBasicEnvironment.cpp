f9aeb546bcabf7f35c6ef1900ec570c401a6c10c
Rainer Hochecker <fernetmenta@online.de>
2017-11-13 20:36:18 +0100

    kill g_Windowing, another nasty global

--- xbmc/test/TestBasicEnvironment.cpp
+++ xbmc/test/TestBasicEnvironment.cpp
@@ -122,6 +122,9 @@ void TestBasicEnvironment::SetUp()
   g_application.m_ServiceManager->CreateAudioEngine();
   CServiceBroker::GetSettings().Initialize();
 
+  std::unique_ptr<CWinSystemBase> winSystem = CWinSystemBase::CreateWinSystem();
+  g_application.m_ServiceManager->SetWinSystem(std::move(winSystem));
+
   if (!g_application.m_ServiceManager->InitStageTwo(CAppParamParser()))
     exit(1);
 
